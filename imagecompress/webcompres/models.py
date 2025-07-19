b7198b541a6e6a404cc6b06facfa24bdd13e3cd3
floralebonnakatumbo <99325795+floralebonna@users.noreply.github.com>
2025-07-19 16:46:09 +0700

    Delete imagecompress/webcompres directory

--- imagecompress/webcompres/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class CompressImage(models.Model):
-    orginal = models.ImageField(upload_to='original/')
-    compres = models.ImageField(upload_to='compres/', null =True, blank=True)
